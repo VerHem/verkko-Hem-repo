7293d60503dc0876ab3d8879dbb6897d136b41e4
timohyva <hyvarinentimo00@gmail.com>
2024-04-04 13:39:23 +0300

    add femgl/inc/BinA.h and femgl/src/setup_2.cc into source tree. They were meant for setup initial configuration of B-phase bubble in A-phase. The codes are still under working. commit for cleanup source tree.

---
+++ femgl/inc/BinA.h
@@ -0,0 +1,123 @@
+/*
+ *
+ */
+
+#ifndef BINA_H
+#define BINA_H
+
+#include <random> // c++ std radom bumber library, for gaussian random initiation
+
+#include <deal.II/base/quadrature_lib.h>
+#include <deal.II/base/function.h>
+#include <deal.II/base/timer.h>
+
+#include <deal.II/lac/generic_linear_algebra.h>
+
+#include <deal.II/lac/vector.h>
+#include <deal.II/lac/full_matrix.h>
+#include <deal.II/lac/solver_cg.h>
+#include <deal.II/lac/solver_gmres.h>
+#include <deal.II/lac/solver_minres.h>
+#include <deal.II/lac/solver_bicgstab.h>
+#include <deal.II/lac/affine_constraints.h>
+#include <deal.II/lac/dynamic_sparsity_pattern.h>
+#include <deal.II/lac/sparsity_tools.h>
+#include <deal.II/lac/sparse_matrix.h>
+#include <deal.II/lac/block_sparsity_pattern.h>
+#include <deal.II/lac/precondition.h>
+#include <deal.II/lac/trilinos_sparse_matrix.h>
+#include <deal.II/lac/trilinos_sparsity_pattern.h>
+#include <deal.II/lac/trilinos_block_sparse_matrix.h>
+#include <deal.II/lac/trilinos_vector.h>
+#include <deal.II/lac/trilinos_parallel_block_vector.h>
+#include <deal.II/lac/trilinos_precondition.h>
+#include <deal.II/lac/trilinos_solver.h>
+
+#include <deal.II/grid/tria.h>
+#include <deal.II/grid/grid_generator.h>
+#include <deal.II/grid/manifold_lib.h>
+#include <deal.II/grid/grid_tools.h>
+
+#include <deal.II/distributed/tria.h>
+#include <deal.II/distributed/grid_refinement.h>
+#include <deal.II/distributed/solution_transfer.h>
+
+#include <deal.II/dofs/dof_handler.h>
+#include <deal.II/dofs/dof_renumbering.h>
+#include <deal.II/dofs/dof_tools.h>
+
+#include <deal.II/fe/fe_values.h>
+#include <deal.II/fe/fe_q.h>
+#include <deal.II/fe/fe_system.h>
+#include <deal.II/fe/component_mask.h>
+
+#include <deal.II/numerics/vector_tools.h>
+#include <deal.II/numerics/data_out.h>
+#include <deal.II/numerics/error_estimator.h>
+#include <deal.II/numerics/matrix_tools.h>
+#include <deal.II/numerics/data_out.h>
+#include <deal.II/numerics/fe_field_function.h>
+
+#include <deal.II/base/utilities.h>
+#include <deal.II/base/conditional_ostream.h>
+#include <deal.II/base/index_set.h>
+#include <deal.II/base/parameter_handler.h>
+#include <deal.II/base/quadrature_lib.h>
+#include <deal.II/base/function.h>
+#include <deal.II/base/timer.h>
+
+#include <cmath>
+#include <fstream>
+#include <iostream>
+
+namespace FemGL_mpi
+{
+  using namespace dealii;
+
+  /* ------------------------------------------------------------------------------------------
+   * class template BinA inhereted from Function<dim>.
+   * set the reference value_list to B-in-A configuration for full-step newton iteration.
+   * ------------------------------------------------------------------------------------------
+   */
+  
+  template <int dim>
+  class BinA : public Function<dim>
+  {
+  public:
+    BinA(double &r)
+      : Function<dim>(18) // tell base Function<dim> class I want a 2-components vector-valued function
+      , radius(r)
+    {}
+
+    double radius; // in unit of \xi_0
+    
+    virtual void vector_value(const Point<dim> & /*p*/,
+                              Vector<double> &values) const override
+    {
+      Assert(values.size() == 18, ExcDimensionMismatch(values.size(), 18));
+
+      if ( (p(0)*p(0) + p(1)*p(1) +p(2)*p(2)) <= (r*r) )
+	{
+         for (auto &value_of_index : values)
+           {
+            
+	   }
+
+	}
+    }
+
+    virtual void
+    vector_value_list(const std::vector<Point<dim>> &points,
+                      std::vector<Vector<double>> &  value_list) const override
+    {
+      Assert(value_list.size() == points.size(),
+             ExcDimensionMismatch(value_list.size(), points.size()));
+
+      for (unsigned int p = 0; p < points.size(); ++p)
+        BinA<dim>::vector_value(points[p], value_list[p]);
+    }
+  };
+
+} // namespace FemGL_mpi
+
+#endif
