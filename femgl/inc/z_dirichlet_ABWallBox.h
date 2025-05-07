0cf34e3dba3b34906cabef95f0bc20c92047c94c
timohyva <hyvarinentimo00@gmail.com>
2025-05-07 19:39:18 +0300

    vector_value() Function class definination header. The class constructor is passed to VectorTool::interpolate() func in setup() func, which is defined in setup_uniform_BnA-flatwall-z-Dirichilet-BC-configuration.cc.

---
+++ femgl/inc/z_dirichlet_ABWallBox.h
@@ -0,0 +1,176 @@
+/* ------------------------------------------------------------------------------------------
+ *
+ * Copyright (C) 2023-present by Kuang. Zhang
+ *
+ * This library is free software; you can redistribute it and/or modify it under 
+ * the terms of the GNU Lesser General Public License as published by the Free Software Foundation; 
+ * either version 2.1 of the License, or (at your option) any later version.
+ *
+ * Permission is hereby granted to use or copy this program under the
+ * terms of the GNU LGPL, provided that the Copyright, this License 
+ * and the Availability of the original version is retained on all copies.
+ * User documentation of any code that uses this code or any modified
+ * version of this code must cite the Copyright, this License, the
+ * Availability note, and "Used by permission." 
+
+ * Permission to modify the code and to distribute modified code is granted, 
+ * provided the Copyright, this License, and the Availability note are retained,
+ * and a notice that the code was modified is included.
+
+ * The third party libraries which are used by this library are deal.II, Triinos and few others.
+ * All components involved third party supports obey their Copyrights, Licence and permissions. 
+ *  
+ * ------------------------------------------------------------------------------------------
+ *
+ * author: Quang. Zhang (timohyva@github), 
+ * Helsinki Institute of Physics, University of Helsinki;
+ * 27. Kesäkuu. 2023.
+ *
+ */
+
+#ifndef Z_DIRICHLET_ABWALLBOX_H
+#define Z_DIRICHLET_ABWALLBOX_H
+
+#include <random> // c++ std radom bumber library, for gaussian random initiation
+
+#include <deal.II/base/quadrature_lib.h>
+#include <deal.II/base/function.h>
+#include <deal.II/base/timer.h>
+
+#include <deal.II/lac/generic_linear_algebra.h>
+
+#include <deal.II/lac/vector.h>
+#include <deal.II/lac/full_matrix.h>
+#include <deal.II/lac/solver_cg.h>
+#include <deal.II/lac/solver_gmres.h>
+#include <deal.II/lac/solver_minres.h>
+#include <deal.II/lac/solver_bicgstab.h>
+#include <deal.II/lac/affine_constraints.h>
+#include <deal.II/lac/dynamic_sparsity_pattern.h>
+#include <deal.II/lac/sparsity_tools.h>
+#include <deal.II/lac/sparse_matrix.h>
+#include <deal.II/lac/block_sparsity_pattern.h>
+#include <deal.II/lac/precondition.h>
+#include <deal.II/lac/trilinos_sparse_matrix.h>
+#include <deal.II/lac/trilinos_sparsity_pattern.h>
+#include <deal.II/lac/trilinos_block_sparse_matrix.h>
+#include <deal.II/lac/trilinos_vector.h>
+#include <deal.II/lac/trilinos_parallel_block_vector.h>
+#include <deal.II/lac/trilinos_precondition.h>
+#include <deal.II/lac/trilinos_solver.h>
+
+#include <deal.II/grid/tria.h>
+#include <deal.II/grid/grid_generator.h>
+#include <deal.II/grid/manifold_lib.h>
+#include <deal.II/grid/grid_tools.h>
+
+#include <deal.II/distributed/tria.h>
+#include <deal.II/distributed/grid_refinement.h>
+#include <deal.II/distributed/solution_transfer.h>
+
+#include <deal.II/dofs/dof_handler.h>
+#include <deal.II/dofs/dof_renumbering.h>
+#include <deal.II/dofs/dof_tools.h>
+
+#include <deal.II/fe/fe_values.h>
+#include <deal.II/fe/fe_q.h>
+#include <deal.II/fe/fe_system.h>
+#include <deal.II/fe/component_mask.h>
+
+#include <deal.II/numerics/vector_tools.h>
+#include <deal.II/numerics/data_out.h>
+#include <deal.II/numerics/error_estimator.h>
+#include <deal.II/numerics/matrix_tools.h>
+#include <deal.II/numerics/data_out.h>
+#include <deal.II/numerics/fe_field_function.h>
+
+#include <deal.II/base/utilities.h>
+#include <deal.II/base/conditional_ostream.h>
+#include <deal.II/base/index_set.h>
+#include <deal.II/base/parameter_handler.h>
+#include <deal.II/base/quadrature_lib.h>
+#include <deal.II/base/function.h>
+#include <deal.II/base/timer.h>
+
+#include <cmath>
+#include <fstream>
+#include <iostream>
+
+/*
+ * ******************************************
+ * A quick doc for this specific non-0 Dirichlet BCs func objects file
+ *
+ * This func objects header contains boundary value handling func objects
+ * usded for solver AB Domain wall configuration in different p-T-H.
+ * The objects defined in this header will be called by VectorTool::interpolate_boundary_values()
+ * for boundary with id 0.
+ *
+ * This header should be included in BnA-flatwall-z-Dirichlet.cc, which muast be used with 
+ * retangle-z-Dirichlet-xy:HomoNeumann.cc grid make func
+ */
+
+namespace FemGL_mpi
+{
+  using namespace dealii;
+
+  /* ------------------------------------------------------------------------------------------
+   * class template BoundaryValues inhereted from Function<dim>.
+   * set the reference value_list as Zero Dirichilet BC.
+   * ------------------------------------------------------------------------------------------
+   */
+  
+  template <int dim>
+  class z_SurfaceABWall_BoundaryValues : public Function<dim>
+  {
+  public:
+    z_SurfaceABWall_BoundaryValues(const double hz, const double matelem_A, const double matelem_B)
+      : Function<dim>(18) // tell base Function<dim> class I want a 2-components vector-valued function
+      , half_z_length(hz)
+      , me_A(matelem_A)
+      , me_B(matelem_B)
+    {}
+
+    double half_z_length, me_A, me_B;
+    
+    virtual void vector_value(const Point<dim> &point /*p*/,
+                              Vector<double> &values) const override
+    {
+      Assert(values.size() == 18, ExcDimensionMismatch(values.size(), 18));
+
+      // All points on id-0 wil have OP element 0
+      for (auto &value_of_index : values)                                                                              
+        value_of_index = 0.0;
+
+      // point at A-phase side a.k.a -half-z-side 
+      if (std::fabs(point(2) - (-half_z_length)) < 1e-12 * half_z_length)
+	{
+	  values[0] = me_A;
+	  values[10] = me_A;	  
+	}
+
+      // point at B-phase side a.k.a half-z-side 
+      if (std::fabs(point(2) - (half_z_length)) < 1e-12 * half_z_length)
+	{
+          values[0] = me_B;
+          values[4] = me_B;
+          values[8] = me_B;	  
+	}
+      
+    }
+
+    virtual void
+    vector_value_list(const std::vector<Point<dim>> &points,
+                      std::vector<Vector<double>> &  value_list) const override
+    {
+      Assert(value_list.size() == points.size(),
+             ExcDimensionMismatch(value_list.size(), points.size()));
+
+      for (unsigned int p = 0; p < points.size(); ++p)
+        z_SurfaceABWall_BoundaryValues<dim>::vector_value(points[p], value_list[p]);
+    }
+  };
+
+ 
+} // namespace FemGL_mpi
+
+#endif
